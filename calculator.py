5a78d9d04a7dd3e01cea2e12c4ab6a347b127ccd
battle1368 <battle1368@gmail.com>
2022-07-06 18:58:47 +0100

    Now takes input through console rather than arguments

--- calculator.py
+++ calculator.py
@@ -1,12 +1,10 @@
 import sys
 import rpncalc
 
-expression = ""
-for i in range(len(sys.argv) - 1):
-	expression = expression + sys.argv[i + 1] + " "
+expression = input("Please enter an expression:\n")
 result = rpncalc.calc(expression)
 if result == ValueError:
-	print("Invalid argument! Expected integer or valid operator.")
+	print("Invalid argument! Expected an integer or a valid operator.")
 elif result == Exception:
 	print("Invalid expression!")
 else:
